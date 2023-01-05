862d92efc8b971073dff4229d43eb581f1e7d4ac
tanluuuuuuu <20521591@gm.uit.edu.vn>
2023-01-05 10:01:06 +0700

    test delete

--- Exchange Market/FormExchange.cs
+++ Exchange Market/FormExchange.cs
@@ -17,11 +17,6 @@ namespace Exchange_Market
             InitializeComponent();
         }
 
-        private void doSomething()
-        {
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
             FormNews newsForm = new FormNews();
